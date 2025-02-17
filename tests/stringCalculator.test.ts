3b81f26dae2550c58fd380322762342955d2cc76
Shrikant <itzshrikant@gmail.com>
2025-02-17 20:50:40 +0530

    test case for handling multiple numbers

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -12,4 +12,8 @@ describe("String Calculator", () => {
   test("returns the sum of two comma-separated numbers", () => {
     expect(add("1,5")).toBe(6);
   });
+
+  test("returns the sum of multiple comma-separated numbers", () => {
+    expect(add("1,2,3,4,5")).toBe(15);
+  });
 });
