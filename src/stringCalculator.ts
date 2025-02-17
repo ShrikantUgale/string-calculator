3fdb3daf1749905b0af5a1abfdfa310b8346e386
Shrikant <itzshrikant@gmail.com>
2025-02-17 21:00:02 +0530

    handled new line character

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,5 +1,11 @@
 export function add(numbers: string) {
   if (!numbers) return 0;
-  const numberArray = numbers.split(",").map((num) => Number(num));
+
+  // Replace newlines with commas
+  const normalizedNumbers = numbers.replace(/\n/g, ",");
+
+  // Convert string to array of numbers
+  const numberArray = normalizedNumbers.split(",").map((num) => Number(num));
+
   return numberArray.reduce((sum, num) => sum + num, 0);
 }
