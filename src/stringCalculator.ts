359902456db607bb2b155b5fae89066f3b0d38f0
Shrikant <itzshrikant@gmail.com>
2025-02-17 20:45:31 +0530

    hnadled for comma separated numbers

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,4 +1,5 @@
 export function add(numbers: string) {
   if (!numbers) return 0;
-  return Number(numbers);
+  const numberArray = numbers.split(",").map((num) => Number(num));
+  return numberArray.reduce((sum, num) => sum + num, 0);
 }
